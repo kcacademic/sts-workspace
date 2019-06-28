56c30d9cd3c6212d7d70ee0076482236fd2dabfb
Kumar Chandrakant <kchandrakant@gmail.com>
2019-06-28 23:28:39 +0530

    Adding daily batch of updates.

---
+++ Spring Projects/spring-redis/src/main/java/com/sapient/learning/Application.java
@@ -0,0 +1,29 @@
+package com.sapient.learning;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.sapient.learning.repository.CustomerRepository;
+
+@SpringBootApplication
+public class Application {
+
+	@SuppressWarnings("unused")
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo(CustomerRepository repository) {
+		return (args) -> {
+
+		};
+	}
+
+}
