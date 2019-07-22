89dc6eb8ba87086d7a1874522614bf35b63fbca3
Kumar Chandrakant <kchandrakant@gmail.com>
2019-07-22 23:03:07 +0530

    Adding daily batch of work.

---
+++ Framework Projects/java-micronaut/src/main/java/com/sapient/learning/Application.java
@@ -0,0 +1,10 @@
+package com.sapient.learning;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
