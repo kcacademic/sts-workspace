0bd132c7e60d22e541b588828174da509dcbccaa
Kumar Chandrakant <kchandrakant@gmail.com>
2019-07-23 16:45:53 +0530

    Adding daily batch of work.

---
+++ Spring Boot Projects/spring-swagger/src/main/java/com/sapient/learning/controller/Controller.java
@@ -0,0 +1,24 @@
+package com.sapient.learning.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.ApiResponse;
+
+@RestController
+@Api(tags = "Sample API")
+public class Controller {
+	
+	@GetMapping("/hello")
+	@ApiOperation(value = "Sample hello world operation!")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "ok"), 
+	                        @ApiResponse(code = 500, message = "An unexpected error occurred") 
+	                      })
+	public String hello() {
+		return "Hello World!";
+	}
+
+}
