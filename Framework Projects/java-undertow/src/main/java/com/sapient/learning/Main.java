531bf1bf4f3ce301c11f47cb76ecc5d823ef5fb6
Kumar Chandrakant <kchandrakant@gmail.com>
2019-08-22 12:25:42 +0530

    Adding daily batch of work.

---
+++ Framework Projects/java-undertow/src/main/java/com/sapient/learning/Main.java
@@ -0,0 +1,20 @@
+package com.sapient.learning;
+
+import io.undertow.Undertow;
+import io.undertow.util.Headers;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Undertow server = Undertow.builder()
+				.addHttpListener(8080, "localhost")
+				.setHandler(exchange -> {
+					exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+					exchange.getResponseSender().send("Hello Undertow!");
+				}).build();
+		server.start();
+
+	}
+
+}
