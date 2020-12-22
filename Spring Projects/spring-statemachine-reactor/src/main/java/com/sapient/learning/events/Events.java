59ba9b3ebc20cbedd8fe838f6cc2100b2d43fe3d
CHANDRAKANT Kumar <kumar.chandrakant@soprabanking.com>
2020-12-22 19:28:05 +0530

    Adding daily batch of work.

---
+++ Spring Projects/spring-statemachine-reactor/src/main/java/com/sapient/learning/events/Events.java
@@ -0,0 +1,5 @@
+package com.sapient.learning.events;
+
+public enum Events {
+    EVENT1, EVENT2
+}
---
+++ Spring Projects/spring-statemachine-reactor/src/main/java/com/sapient/learning/states/States.java
@@ -0,0 +1,5 @@
+package com.sapient.learning.states;
+
+public enum States {
+    STATE1, STATE2
+}
