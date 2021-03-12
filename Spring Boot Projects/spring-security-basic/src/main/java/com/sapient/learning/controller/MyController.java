f76fdf2d92615c6a535672410bbaa11543f0f5d4
CHANDRAKANT Kumar <kumar.chandrakant@soprabanking.com>
2021-03-13 02:12:21 +0530

    Adding daily batch of work.

---
+++ Spring Boot Projects/spring-security-basic/src/main/java/com/sapient/learning/controller/MyController.java
@@ -0,0 +1,24 @@
+package com.sapient.learning.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+	@GetMapping("/api/user")
+	@PreAuthorize("hasRole('ROLE_USER')")
+	public String getUserGreeting() {
+		System.out.println("Method getUserGreeting() invoked.");
+		return "Hello User!";
+	}
+
+	@GetMapping("/api/admin")
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	public String getAdminGreeting() {
+		System.out.println("Method getAdminGreeting() invoked.");
+		return "Hello Admin!";
+	}
+
+}
